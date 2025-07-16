de481f68d39b2a617651b793a7cd118b67d53ebc
Binod X <160247882+binnoodx@users.noreply.github.com>
2025-07-16 15:05:52 +0545

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,6 @@
 // socket.js (or socket.ts)
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-backend-ypo5.onrender.com/", {
+export const socket = io("https://chat-backend-ypo5.onrender.com", {
   transports: ["websocket"],
 });
