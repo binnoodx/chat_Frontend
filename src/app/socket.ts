cb585e20c740b1c9be8181f3285b3f7fdb32936b
Binod Sharma <thebinod404@gmail.com>
2025-07-16 14:41:30 +0545

    Add Backend URL

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,6 @@
 // socket.js (or socket.ts)
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io("https://chat-backend-ypo5.onrender.com/", {
   transports: ["websocket"],
 });
